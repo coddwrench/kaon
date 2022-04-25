e6b3f154e9f5ff10955997737b3d7a2c4e9de731
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2022-04-25 04:17:29 +0400

    Add unit tests
    Use google C++ tests framework.

---
+++ tests/log.cpp
@@ -0,0 +1,36 @@
+#include <memory>
+#include <fstream>
+#include <cstdio>
+#include <log.hpp>
+#include <gtest/gtest.h>
+
+class logTest : public ::testing::Test {
+  protected:
+    static constexpr const char *mLogFile = "logTest.log";
+    std::shared_ptr<log> mLoggerPtr;
+
+    logTest() {
+      mLoggerPtr.reset(new log("logTest", mLogFile));
+    }
+    ~logTest() override {
+//      std::remove(mLogFile);
+    }
+};
+
+TEST_F(logTest, stdoutTest) {
+  std::time_t time = std::time(nullptr);
+  std::string localtime = std::asctime(std::localtime(&time));
+  localtime.erase(--localtime.end(), localtime.end());
+  LOG(*mLoggerPtr, "leel");
+
+  std::ifstream logFile(mLogFile);
+  std::string str = "[" + localtime + "] logTest: leel";
+  std::string logStr;
+  std::getline(logFile, logStr);
+  ASSERT_EQ(logStr, str);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
