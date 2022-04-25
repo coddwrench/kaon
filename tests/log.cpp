fc80305a80d302eeeaa5904d880070da59e04ea0
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2022-04-25 04:51:53 +0400

    Add banner

--- tests/log.cpp
+++ tests/log.cpp
@@ -1,3 +1,10 @@
+/*
+ * vim:et:sw=2:ts=2
+ *
+ * Copyright (c) 2022 Nikita (sh1r4s3) Ermakov <sh1r4s3@mail.si-head.nl>
+ * SPDX-License-Identifier: MIT
+ */
+
 #include <memory>
 #include <fstream>
 #include <cstdio>
