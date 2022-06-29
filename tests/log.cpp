84af7f53f7ac9da1f9b33a112a76d2c9291320d1
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2022-06-30 00:20:57 +0300

    Update logger tests
    - Update logger tests.
    - Add two more tests to check that Logger can handle multiple
      operator<<() and multiple lines.

--- tests/log.cpp
+++ tests/log.cpp
@@ -12,26 +12,60 @@
 #include <gtest/gtest.h>
 
 class LogTest : public ::testing::Test {
-  protected:
-    static constexpr const char *mLogFile = "LogTest.log";
-    std::shared_ptr<Log> mLoggerPtr;
-
+  public:
     LogTest() {
-      mLoggerPtr.reset(new Log("LogTest", mLogFile));
+      Logger::instance().setOutput(mLogFile);
     }
-    ~LogTest() override {
+
+    ~LogTest() {
       std::remove(mLogFile);
     }
+  protected:
+    static constexpr const char *mLogFile = "/tmp/LogTest.log";
 };
 
 TEST_F(LogTest, stdoutTest) {
   std::time_t time = std::time(nullptr);
   std::string localtime = std::asctime(std::localtime(&time));
   localtime.erase(--localtime.end(), localtime.end());
-  LOG(*mLoggerPtr, "leel");
+  LOG("leel");
 
   std::ifstream logFile(mLogFile);
-  std::string str = "[" + localtime + "] LogTest: leel";
+  std::string str = "[" + localtime + "] leel";
+  std::string logStr;
+  std::getline(logFile, logStr);
+  ASSERT_EQ(logStr, str);
+}
+
+TEST_F(LogTest, multipleOutput) {
+  std::time_t time_foo = std::time(nullptr);
+  std::string localtime_foo = std::asctime(std::localtime(&time_foo));
+  localtime_foo.erase(--localtime_foo.end(), localtime_foo.end());
+  LOG("foo");
+
+  std::time_t time_bar = std::time(nullptr);
+  std::string localtime_bar = std::asctime(std::localtime(&time_bar));
+  localtime_bar.erase(--localtime_bar.end(), localtime_bar.end());
+  LOG("bar");
+
+  std::ifstream logFile(mLogFile);
+  std::string str_foo = "[" + localtime_foo + "] foo";
+  std::string str_bar = "[" + localtime_bar + "] bar";
+  std::string logStr_foo, logStr_bar;
+  std::getline(logFile, logStr_foo);
+  std::getline(logFile, logStr_bar);
+  ASSERT_EQ(logStr_foo, str_foo);
+  ASSERT_EQ(logStr_bar, str_bar);
+}
+
+TEST_F(LogTest, multipleOperator) {
+  std::time_t time = std::time(nullptr);
+  std::string localtime = std::asctime(std::localtime(&time));
+  localtime.erase(--localtime.end(), localtime.end());
+  LOG("foo" << " bar");
+
+  std::ifstream logFile(mLogFile);
+  std::string str = "[" + localtime + "] foo bar";
   std::string logStr;
   std::getline(logFile, logStr);
   ASSERT_EQ(logStr, str);
